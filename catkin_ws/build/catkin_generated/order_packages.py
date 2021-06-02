9ac48e12b3cc125aab68bb545a923f2ca8b8ad6a
George Krommydas <43825468+GeoKrom@users.noreply.github.com>
2021-06-03 00:14:17 +0300

    Add files via upload

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/usr/home/students/stud16/cse63260/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/usr/home/students/stud16/cse63260/catkin_ws/devel;/opt/ros/melodic".split(';') if "/usr/home/students/stud16/cse63260/catkin_ws/devel;/opt/ros/melodic" != "" else []
